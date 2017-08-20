c97a9c4b252d6a676daeae75bca5d882ca44e28e
Ernest Mallett <ernest.e.mallett2@gmail.com>
2017-08-20 16:14:26 -0500

    Visual Update

--- AboutDialog.Designer.cs
+++ AboutDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutDialog));
             this.closeButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -69,6 +70,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.closeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AboutDialog";
             this.Text = "About";
             this.ResumeLayout(false);
